364762b6a075eabc8a5941af12fff5cb252c9459
Tim Endruszeit <tim.endruszeit@moguru.de>
2019-11-24 15:40:36 +0100

    App Bar Theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'env.dart';
 import 'open_weather_map.dart';
 import 'package:location/location.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   await Env.init();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent, statusBarIconBrightness: Brightness.dark));
   runApp(MyApp());
 }
 
@@ -16,6 +18,10 @@ class MyApp extends StatelessWidget {
       title: 'Weather',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          brightness: Brightness.light
+        )
       ),
       home: WeatherPage(title: 'City'),
     );
@@ -27,12 +33,12 @@ class WeatherPage extends StatefulWidget {
   final String title;
 
   @override
-  _WeatherPageState createState() => _WeatherPageState(title: title);
+  _WeatherPageState createState() => _WeatherPageState(cityName: title);
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-  _WeatherPageState({this.title});
-  String title;
+  _WeatherPageState({this.cityName});
+  String cityName;
   bool _isLoading = false;
   Weather weather;
   LocationData currentLocation;
@@ -51,7 +57,7 @@ class _WeatherPageState extends State<WeatherPage> {
       final openWeather = OpenWeatherMap(apiKey: Env.openWeatherMapApiKey);
       weather = await openWeather.fetchWeatherByLocation(
           latitude: currentLocation.latitude , longitude: currentLocation.longitude, units: "metric");
-      title = weather.cityName;
+      cityName = weather.cityName;
     } catch (e) {
       print(e.toString());
     } finally {
@@ -71,13 +77,18 @@ class _WeatherPageState extends State<WeatherPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: FancyShimmerText(Text("Premium Weather Deluxe Edition")),
         actions: [
           IconButton(
-            icon: Icon(Icons.refresh),
+            icon: Icon(Icons.refresh, color: Colors.black54,),
             onPressed: _isLoading ? null : fetchWeather,
           )
         ],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(25)
+          )
+        ),
       ),
       body: Center(
         child: _isLoading
@@ -94,22 +105,41 @@ class WeatherPageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return weather != null ?
+    Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        weather != null ? Image.network(weather.icon) : Container(),
-        Shimmer.fromColors(
-            baseColor: Colors.lightBlue,
-            highlightColor: Colors.purpleAccent,
-            child:  Text(
-              (weather != null ? weather.temperature.toString() : "?") + "°",
+        Image.network(weather.icon),
+        FancyShimmerText(Text(
+              weather.temperature.toString() + "°",
               style: TextStyle(
                 fontSize: 50,
                 fontWeight: FontWeight.bold,
               ),
-            ),
+            )
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.location_on),
+            Text(weather.cityName)
+          ],
         )
       ],
+    ) : Container();
+  }
+}
+
+class FancyShimmerText extends StatelessWidget {
+  FancyShimmerText(this.text);
+  final Text text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.lightBlue,
+      highlightColor: Colors.purpleAccent,
+      child: text
     );
   }
 }
